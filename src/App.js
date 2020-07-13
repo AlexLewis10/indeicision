611aa022b898fdf5c86af7aefdfc8042ccf1ad8e
Alex Lewis <alexlewis374@gmail.com>
2020-07-13 17:43:01 +0100

    Define First name arrow function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Home from './components/home'
 
+const getFirstName = (name) => name.split(' ')[0]
+
+
+console.log(getFirstName("Alex Lewis"))
+
 function App() {
   return (
     <div>
