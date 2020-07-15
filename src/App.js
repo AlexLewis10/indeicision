ac73c2864bcf0eab264fb582351e46b3b6ed96f3
Alex Lewis <alexlewis374@gmail.com>
2020-07-15 12:53:42 +0100

    Add form submit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
 import React from 'react';
 import Home from './components/home'
 
-let count = 0
-const addOne = () => {
-  // count += 1
-  console.log('+ 1')
-}
 
-const minusOne = () => {
-  console.log('-1')
-}
 
-const reset = () => {
-  console.log('reset')
-}
-
-const template = (
-  <div>
-    <h1>Count: {count}</h1>
-    <button onClick={addOne}>+1</button>
-    <button onClick={minusOne}>-1</button>
-    <button onClick={reset}>Reset</button>
-  </div>
-)
 
 
 function App() {
   return (
     <div>
       <Home />
-      {template}
     </div>
   );
 }
--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 export class Home extends Component {
+  
   render() {
 
     const app = { 
@@ -13,10 +14,24 @@ export class Home extends Component {
       return app.options.length > 0 ? 'Here are your options' : 'No options'
     };
 
+    const onFormSubmit = (e) => {
+      e.preventDefault()
+    }
+
     return (
+
       <div>
+        <h1>{app.title}</h1>
         {(app.subtitle && <p>Subtitle: {app.subtitle}</p>)}
         <p>{renderOptions()}</p>
+        <ol>
+          <li>Item one</li>
+          <li>Item two</li>
+        </ol>
+        <form onSubmit={onFormSubmit}>
+          <input type="text" name="option"/>
+          <button>Submit</button>
+        </form>
       </div>
     )
   }
