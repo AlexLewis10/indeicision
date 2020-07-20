5768d193271de3edeac47d35f9a55c9fd97e84a3
Alex Lewis <alexlewis374@gmail.com>
2020-07-20 14:46:42 +0100

    Add Option component in Options

--- src/components/options.js
+++ src/components/options.js
@@ -3,10 +3,19 @@ import Option from './option'
 
 
 export class Options extends Component {
+
+
+    
+
+
+
   render() {
     return (
       <div>
         <p>You have {this.props.options.length} Options</p>
+        <div>
+        {this.props.options.map((option) => <p key={option}>{option}</p>)}
+        </div>
         <Option />
       </div>
     )
