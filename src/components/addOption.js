79895255bf98aca7c38f304d81695e356824f357
Alex Lewis <alexlewis374@gmail.com>
2020-07-21 12:32:43 +0100

    Add form to add option

--- src/components/addOption.js
+++ src/components/addOption.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 
 export class AddOption extends Component {
+
+  handleAddOption (e) {
+    e.preventDefault()
+    const option = e.target.elements.addOption.value
+    if (option) {
+      alert('Add Option')
+    }
+  }
   render() {
     return (
       <div>
-        <p>Add options</p>
+        <form onSubmit={this.handleAddOption}>
+          <input type='text' name='addOption' />
+          <button>Submit</button>
+        </form>
       </div>
     )
   }
