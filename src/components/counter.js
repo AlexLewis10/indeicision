5bedf523e4f0d11f3dee507d1ee11cb54ac9fa51
Alex Lewis <alexlewis374@gmail.com>
2020-07-22 14:18:46 +0100

    Add state

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,32 +1,32 @@
 import React, { Component } from 'react'
 
 export class Counter extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.handleAddOne = this.handleAddOne.bind(this)
+    this.handleMinusOne = this.handleMinusOne.bind(this)
+    this.handleReset = this.handleReset.bind(this)
     this.state = {
       count: 0
     }
   }
 
-  handleAddOne = () => {
-    
+  handleAddOne() {
+    this.setState({ count: this.state.count += 1})
   }
 
   handleMinusOne() {
-
+    this.setState({ count: this.state.count -= 1})
   }
 
   handleReset() {
-
+    this.setState({ count: 0 })
   }
 
-
- 
   render() {
     return (
       <div>
-        <h1>Count: </h1>
+        <h1>Count: {this.state.count}</h1>
         <button onClick={this.handleAddOne}>+1</button>
         <button onClick={this.handleMinusOne}>-1</button>
         <button onClick={this.handleReset}>Reset</button>
