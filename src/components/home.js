13fe76461e449de20a5094895c7a69f1224ecc9b
Alex Lewis <alexlewis374@gmail.com>
2020-08-18 12:32:35 +0100

    Update syntax for Home component

--- src/components/home.js
+++ src/components/home.js
@@ -6,16 +6,34 @@ import AddOption from './addOption'
 import Counter from './counter'
 
 export class Home extends Component {
-  constructor(props) {
-    super(props)
-    this.handleDeleteOptions = this.handleDeleteOptions.bind(this)
-    this.handlePick = this.handlePick.bind(this)
-    this.handleAddOption = this.handleAddOption.bind(this)
-    this.handleDeleteOption = this.handleDeleteOption.bind(this)
-      this.state = {
-        options: []
-      }
+  state = {
+    options: []
+  }
+
+  handleDeleteOptions = () => {
+    this.setState({ options: [] })
+  }
+
+  handlePick = () => {
+    const randomNum = Math.floor(Math.random() * this.state.options.length)
+    const option = this.state.options[randomNum]
+    alert(option)
+  }
+
+  handleAddOption = (option) => {
+    if (!option) {
+      return 'Enter valid value to add item'
+    } else if (this.state.options.indexOf(option) > -1){
+      return 'This option already exists'
     }
+    this.setState((prevState) => ({ options: prevState.options.concat([option])}))
+  }
+
+  handleDeleteOption = (optionToRemove) => {
+    this.setState((prevState) => ({
+      options: prevState.options.filter((option) => optionToRemove !== option )
+    }))
+  }
 
     componentDidMount() {
       try {
@@ -28,7 +46,6 @@ export class Home extends Component {
       } catch (e) {
         // Do nothing
       }
-
     }
 
     componentDidUpdate(prevProps, prevState) {
@@ -42,31 +59,6 @@ export class Home extends Component {
 
     componentWillUnmount() {
       alert('component will unmount')
-    }
-
-    handleDeleteOptions() {
-      this.setState({ options: [] })
-    }
-
-    handlePick() {
-      const randomNum = Math.floor(Math.random() * this.state.options.length)
-      const option = this.state.options[randomNum]
-      alert(option)
-    }
-
-    handleAddOption(option) {
-      if (!option) {
-        return 'Enter valid value to add item'
-      } else if (this.state.options.indexOf(option) > -1){
-        return 'This option already exists'
-      }
-      this.setState((prevState) => ({ options: prevState.options.concat([option])}))
-    }
-
-    handleDeleteOption(optionToRemove) {
-      this.setState((prevState) => ({
-        options: prevState.options.filter((option) => optionToRemove !== option )
-      }))
     }
 
     render() {
